1fd776e54e0bb60eea89c7c494456d8b88b9c051
Alejandro Lopez <negocios.alejandrolopezmurillo@gmail.com>
2026-08-27 21:17:42 -0500

    UI: update button source text to New Order for better english translation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -44,7 +44,7 @@
 
                 <div class="mt-8 flex flex-wrap justify-center gap-3">
                     <a href="{{ route('request') }}" class="btn-primary px-6 py-3 text-base">
-                        {{ __('Request a Purchase') }}
+                        {{ __('New Order') }}
                         <svg class="h-4.5 w-4.5" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3" />
                         </svg>
